cda343015659f9f8801f91045baa1691ad9994c4
Thinking Song <123wuming_123@163.com>
2016-04-05 17:56:56 +0800

    update config server

--- config-server/src/main/java/com/wmstars/config/ConfigApplication.java
+++ config-server/src/main/java/com/wmstars/config/ConfigApplication.java
@@ -1,9 +1,13 @@
 package com.wmstars.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableConfigServer
 public class ConfigApplication {
 
 	public static void main(String[] args) {
